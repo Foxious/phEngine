629119cfe8600b74eaa059409b2e0c08385b0310
Toli <drachis+git@gmail.com>
2012-12-15 21:18:38 -0800

    experimenting with dungeon stuff

--- proto/DungeonGenerator.py
+++ proto/DungeonGenerator.py
@@ -7,6 +7,52 @@ C:\Users\Drachis\.spyder2\.temp.py
 """
 import random
 import math
+class point():
+    def __init__(self,position = (0,0)):
+        self.position = position
+
+class room():
+    def __init__(self):
+        #generate a room that will conect to a number of other rooms based on the number of exits it has
+        self.exits = self.numberOfExits()
+        self.neighbors = []
+    def numberOfExits(self,maximum=4):
+        #sugessted maximum of 8
+        self.exits = random.randint(1,maximum)
+        
+        
+class dungeon():
+    def __init__(self):
+        self.roomNum= self.numberOfRooms()
+        self.rooms = []
+        self.fillDungeon()
+        
+    def numberOfRooms(self,max=13):
+        return random.randint(5,max)
+    def fillDungeon(self):
+        i = 0 
+        while  i < self.roomNum:
+            i += 1
+            cell = room()
+            if cell.exits >= 2:
+                neighbors = 0
+                while neighbors < cell.exits:
+                    neighbors += 1
+                    i += 1
+                    cell.neighbors.append(room())
+            self.rooms.append(cell)
+    def placeRoom(self, cell):
+        while self.roomNum < len(self.rooms) and len(cell.neighbors) < cell.exits:
+            pass
+        cell = room()
+        if cell.exits >= 2:
+            neighbors = 0
+            while neighbors < cell.exits:
+                neighbors += 1
+                i += 1
+                cell.neighbors.append(room())
+        self.rooms.append(cell)        
+            
 
 def makeRoom(size):
     room = list()
@@ -30,13 +76,6 @@ def generateRow(size):
         dungeon.append(generateCell())
     return dungeon
 
-if __name__ == '__main__':
-    for row in makeRoom(8):
-        hor = ''
-        for cell in row:
-            hor += cell + ' '
-        print hor
-
 def selectRoom(roomlist):
     placed = []
     while len(placed) <= len(roomlist):
@@ -49,12 +88,18 @@ def selectRoom(roomlist):
 def selectRoom(rooms):
     return rooms[int(random.random()*len(rooms))]
 
-if __name__ == '__main__':
-    rooms = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    ]
-    print selectRoom(rooms)
+class floor():
+    def __init__(self,size = (3,3)):
+        
+        for i, value in size:
+            #size should only be holding a set of integers each greater than 3
+            if 3 > value :
+                size[i] = 3
+        self.size = size
+        #initial spawn is in the center of the floor
+        self.entrance = point(
+            position=(
+                random.randint(0, size[0]-1),
+                random.randint(0, size[1]-1)
+            )
+        )
