9811cb7ded512a83f9f035bd751136e483b825f5
Toli Carter <drachis@gmail.com>
2012-12-25 00:32:37 -0800

    attempting to get rooms linking up logically,
    I'm running into some issues with bidirectional linking and creating a
    complete dungeon, I think I will switch to something simpler, I'm doing
    a lot with minimal effewct right now, but the linked objects are
    interested at the moment. ... help?

--- proto/dungeons.py
+++ proto/dungeons.py
@@ -1,42 +1,62 @@
 import random as r
 
 class Room():
-    def __init__(self):
-        self.doors = self.numRooms(1,4)
+    def __init__(self,name=None):
+        self.doors = self.numRooms()
         self.usedDoors = 0
         self.neighbors = []
-    def numRooms(self,num = 2 , maxConnections=4):
+        if name != None:
+            self.name = name
+    def numRooms(self,num = 2 , maxConnections=3):
         if num > maxConnections:
             num = maxConnections
         number =  r.randint(num,maxConnections)
         return number
+    
 def generateDungeon(numberOfRooms = 5):
     roomsToGenerate = numberOfRooms
     rooms = []
     while roomsToGenerate > 0:
-        rooms.append(Room())
+        rooms.append(Room(name=str(numberOfRooms - roomsToGenerate)))
         roomsToGenerate -= 1
     for room in rooms:   
-        for index in range(room.doors):
-            if room.usedDoors <= room.doors:
-                _room = pickUnusedRoom(room,rooms)
-                room.neighbors.append(_room)
-                room.usedDoors += 1
-        print len(room.neighbors)
-def pickUnusedRoom(room = None,rooms = None):
-    #expects a room object and a list of rooms
+        if len(room.neighbors) < room.doors:
+            _room = pickUnusedRoom(room,rooms)
+            room.neighbors.append(_room)
+            if _room != None:
+                if room not in _room.neighbors and room.doors > len(room.neighbors):
+                    _room.neighbors.append(room)
+        #print len(room.neighbors)
+    return rooms
+def pickUnusedRoom(_Room = None,rooms = None):
+    #expects an instance of the Room object and a list of rooms
     #otherwise it fails
-    if room != None and rooms != None:
+    #randomly selects form the list of rooms not in use
+    if _Room != None and rooms != None:
         avalableRooms = []
         for _room in rooms:
-            if _room not in room.neighbors:
-                avalableRooms.append(_room)
-        return avalableRooms[r.randint(0,len(avalableRooms)-1)]
+            if _room not in _Room.neighbors:
+                if _room is not _Room:
+                    if len(_room.neighbors) < _room.doors:
+                       avalableRooms.append(_room)
+        if len(avalableRooms) > 0:
+            returnRoom = avalableRooms[r.randint(0,len(avalableRooms)-1)]
+            return returnRoom
+        else:
+            newRoom = Room(name = 'deadend')
+            print newRoom
+            return newRoom
     else:
         return none
+def testWalker(dungeon):
+  for room in dungeon:
+      neighbors = []
+      for connection in room.neighbors:
+          if room != None and connection != None:
+              neighbors.append(connection.name)
+      print room.name,room.doors,neighbors
 if __name__ =='__main__':
-    test = generateDungeon(5)
-    print test
+    testWalker(generateDungeon(10))
     '''
     generate pool of rooms
     create room connections
