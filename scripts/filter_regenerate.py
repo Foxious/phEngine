743520c775fb3f46c42e8f55ec345142d731e92e
Toli Carter <drachis@gmail.com>
2013-05-07 22:22:53 -0700

    grabbing the best path by comparing the original path string with the potential new locations, generally closest comes in first...should be safe (totally nto safe) but not locked into keywords

--- scripts/filter_regenerate.py
+++ scripts/filter_regenerate.py
@@ -6,6 +6,9 @@ Created Tuesday May 7 18:53 2013
 
 import os
 import xml.etree.ElementTree as ET
+import difflib
+import keyword
+
 
 def find_files(root,search):
     '''
@@ -28,23 +31,30 @@ def parse_xml(xml_file):
         for z in x.attrib.keys():
             if z == "Include":
                 f_path = x.attrib["Include"]
+                if path.find(".") != -1: #check if its a file path
                 root = os.path.split(xml_file)[0]
                 find_path(x,root,f_path) 
             elif z == "Exclude":
                 f_path = x.attrib["Exclude"]
 
 def find_path(entry,root,path):
-    if path.find(".") != -1: #this is a guess, since most Included files have an extension
-        abs_path = os.path.abspath(os.path.join(root,path))
-        abs_root = os.path.abspath(root)
-        if os.path.exists(abs_path) == True:
-            print 'no changes' , path
-            return path
-        else:
-            split_path = os.path.split(path)
-            paths = find_files("..\\",split_path[1])
-            print 'path update:' , paths
-            return paths[0]
+    '''
+    either returns the same path or the best matched path if the path has changed
+    '''
+    abs_path = os.path.abspath(os.path.join(root,path))
+    abs_root = os.path.abspath(root)
+    if os.path.exists(abs_path) == True:
+        #print 'no changes:' , path
+        return path
+    else:
+        split_path = os.path.split(path)
+        paths = find_files("..\\",split_path[1])
+        if len(paths)>1:
+            matches = difflib.get_close_matches(path,paths)
+            paths = matches
+        #print 'potential paths:',paths
+        #the first path is the best match, let's take that
+        return paths[0]
 
 def files_conversion(folder,search):
     for filter_path in find_files(folder,search):
