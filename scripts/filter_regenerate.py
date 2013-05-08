02aff99d2c95ed6d5bd25f7e8693c843f0a7cc1c
Toli Carter <drachis@gmail.com>
2013-05-07 20:20:59 -0700

    got some data back from XML, on the path to making this work

--- scripts/filter_regenerate.py
+++ scripts/filter_regenerate.py
@@ -7,13 +7,32 @@ Created Tuesday May 7 18:53 2013
 import os
 import xml.etree.ElementTree as ET
 
-def find_filters(root):
+def find_files(root,search):
+    '''
+    walk a directory tree and return the files with name substrings matching search
+    returns found files as file objects in mode 'r'
+    '''
     directory = os.walk(root)
-    filters = list()
+    vc_filters = list()
     for folders in directory:
-        for files in folders[2]:
-            filters = [vc_filter for vc_filter in files if vc_filter.split("filter") > 1]
+        filters = [os_file for os_file in folders[2] if os_file.find(search) != -1]
+        if len(filters) != 0:
+            for vc_filter in filters:
+                vc_filters.append(folders[0] + '\\'+ vc_filter)
+    print vc_filters
+    return vc_filters
 
+def parse_xml(xml_file):
+    xml_tree = ET.parse(xml_file)
+    root = xml_tree.getiterator()
+    for x in root:
+        print 'attr: ',,'\ntext: ', x.text
 
+def files_conversion(folder,search):
+    for filter_path in find_files(folder,search):
+        print filter_path
+        parse_xml(filter_path)
+    
+    
 if __name__ == "__main__":
-    find_filters("../")
+    files_conversion("..\\",".filters")
